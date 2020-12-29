5e546e8ab6f79ae7cd7b55fadaa653362a03492e
unknown <yudhistira013@gmail.com>
2020-12-29 14:49:11 +0700

    first push

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class Pages extends BaseController
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	//--------------------------------------------------------------------
+
+}
